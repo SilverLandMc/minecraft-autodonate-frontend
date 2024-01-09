2b5eb27467d38ba565dd7e577bce6c4a9cf48188
Nikita Aushev <arhonist@gmail.com>
2024-01-09 19:14:09 +0500

    Объект для ссылок в навбаре и генерация элементов ссылок с его использованием. Выделение активной страницы в навбаре

--- src/features/NavBar/ui/NavBar.tsx
+++ src/features/NavBar/ui/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, MouseEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AppRoutes as AppRoute, RoutePath } from 'shared/config/routeConfig/routeConfig';
 import homeIcon from './images/homeIcon.png';
 import ranksIcon from './images/ranksIcon.png';
@@ -14,58 +14,38 @@ interface Props {
     closing?: boolean;
 }
 
+type NavBarLink = { icon: string; verboseName: string };
+
+const navBarLinks: Record<AppRoute, NavBarLink> = {
+    [AppRoute.MAIN]: { icon: homeIcon, verboseName: 'Главная' },
+    [AppRoute.RANKS]: { icon: ranksIcon, verboseName: 'Ранги' },
+    [AppRoute.BOOSTERS]: { icon: boostersIcon, verboseName: 'Бустеры' },
+    [AppRoute.CHESTS]: { icon: chestsIcon, verboseName: 'Сундуки' },
+    [AppRoute.RESOURCES]: { icon: resourcesIcon, verboseName: 'Ресурсы' }
+};
+
 const NavBar: FunctionComponent<Props> = ({ onClose, closing: isClosing }) => {
+    const currentPath = useLocation().pathname;
+
     return (
         <div className={classNames(styles.navBar, { [styles.isClosing]: isClosing })}>
-            <div className={styles.linkContainer}>
-                <div className={styles.iconContainer}>
-                    <img src={homeIcon} className={styles.icon} alt="Главная" />
+            {Object.entries(navBarLinks).map(([appRoute, { icon, verboseName }]) => (
+                <div className={styles.linkContainer}>
+                    <div className={styles.iconContainer}>
+                        <img src={icon} className={styles.icon} alt={verboseName} />
+                    </div>
+
+                    <Link
+                        to={RoutePath[appRoute as AppRoute]}
+                        className={classNames(styles.link, {
+                            [styles.isActive]: currentPath === RoutePath[appRoute as AppRoute]
+                        })}
+                        onClick={onClose}
+                    >
+                        {verboseName}
+                    </Link>
                 </div>
-
-                <Link to={RoutePath[AppRoute.MAIN]} className={styles.link} onClick={onClose}>
-                    Главная
-                </Link>
-            </div>
-
-            <div className={styles.linkContainer}>
-                <div className={styles.iconContainer}>
-                    <img src={ranksIcon} className={styles.icon} alt="Ранги" />
-                </div>
-
-                <Link to={RoutePath[AppRoute.RANKS]} className={styles.link} onClick={onClose}>
-                    Ранги
-                </Link>
-            </div>
-
-            <div className={styles.linkContainer}>
-                <div className={styles.iconContainer}>
-                    <img src={boostersIcon} className={styles.icon} alt="Бустеры" />
-                </div>
-
-                <Link to={RoutePath[AppRoute.BOOSTERS]} className={styles.link} onClick={onClose}>
-                    Бустеры
-                </Link>
-            </div>
-
-            <div className={styles.linkContainer}>
-                <div className={styles.iconContainer}>
-                    <img src={chestsIcon} className={styles.icon} alt="Сундуки" />
-                </div>
-
-                <Link to={RoutePath[AppRoute.CHESTS]} className={styles.link} onClick={onClose}>
-                    Сундуки
-                </Link>
-            </div>
-
-            <div className={styles.linkContainer}>
-                <div className={styles.iconContainer}>
-                    <img src={resourcesIcon} className={styles.icon} alt="Ресурсы" />
-                </div>
-
-                <Link to={RoutePath[AppRoute.RESOURCES]} className={styles.link} onClick={onClose}>
-                    Ресурсы
-                </Link>
-            </div>
+            ))}
         </div>
     );
 };
