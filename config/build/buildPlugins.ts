570c03de4793261d500ffb2c76bd15756715e2bb
Nikita Aushev <arhonist@gmail.com>
2024-01-16 23:52:22 +0500

    Husky и pre-commit хук. webpack-bundle-analyzer и команда analyze. Команда валидации typescript. Полное обновление зависимостей.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,19 +1,17 @@
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import Dotenv from "dotenv-webpack";
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import Dotenv from 'dotenv-webpack';
 
-export const buildPlugins = ({
-  paths,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HTMLWebpackPlugin({
-    template: paths.html,
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: "css/[name].[contenthash:8].css", // названия для файлов css
-    chunkFilename: "css/[name].[contenthash:8].css", // названия для чанков при асинхронной подгрузке
-  }),
-  new Dotenv(),
+export const buildPlugins = ({ paths }: BuildOptions): webpack.WebpackPluginInstance[] => [
+    new HTMLWebpackPlugin({
+        template: paths.html
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css', // названия для файлов css
+        chunkFilename: 'css/[name].[contenthash:8].css' // названия для чанков при асинхронной подгрузке
+    }),
+    new Dotenv()
 ];
