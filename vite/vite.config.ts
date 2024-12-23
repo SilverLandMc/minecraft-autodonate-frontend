c742c1fc8cab7c0dba08df3370540732b17146ca
Aushev Nikita <arhonist@gmail.com>
2024-12-24 00:19:19 +0500

    Vite's implementation, stage 2. Deleted webpack-related configs.

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,23 +1,30 @@
 /// <reference types="vitest" />
 import { resolve } from 'node:path';
-import { ConfigEnv, UserConfigExport } from 'vite';
+import { ConfigEnv, loadEnv, UserConfigExport } from 'vite';
 import getPlugins from './utils/plugins/getPlugins';
 import getProxy from './utils/proxy/getProxy';
 
 const envDirName = 'environments';
 
 const defineConfig = ({ mode, command }: ConfigEnv): UserConfigExport => {
-    // if (!isPreview) {
-    //     validateEnvironment({ envDirName, mode });
-    // }
+    const env = loadEnv(mode, envDirName);
+    const apiHost = env.VITE_API_HOST || '/api/v1'; // значение по умолчанию
+    const proxyTarget = env.VITE_PROXY_TARGET || 'https://silverland.fun/'; // значение по умолчанию
 
+    const isDev = mode === 'development';
     const isProxyRequired = command === 'serve' && mode !== 'vitest';
     const proxy = isProxyRequired ? getProxy() : undefined;
 
     return {
         plugins: getPlugins(command),
         envDir: envDirName,
-        define: { 'process.env': {} },
+
+        define: {
+            'process.env': {},
+            __IS_DEV__: isDev,
+            __API_HOST__: JSON.stringify(apiHost),
+            __PROXY_TARGET__: JSON.stringify(proxyTarget)
+        },
         server: {
             proxy,
             port: 3000,
@@ -33,6 +40,12 @@ const defineConfig = ({ mode, command }: ConfigEnv): UserConfigExport => {
             outDir: 'build',
             sourcemap: 'hidden'
         },
+        resolve: {
+            alias: {
+                app: resolve(__dirname, '../src/app')
+            }
+        },
+        // @ts-ignore
         test: {
             include: ['**/__tests__/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
             environment: 'jsdom',
