a5cb973eb3a6c006c3dbd93478403243af07a327
Aushev Nikita <arhonist@gmail.com>
2025-01-04 23:21:53 +0500

    Deleted babel config. Fixed index html file. Deleted webpack config file. Fixed test command.

--- src/shared/ui/Button/__tests__/Button.tsx
+++ src/shared/ui/Button/__tests__/Button.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import Button from './Button';
+import { describe, expect, it } from 'vitest';
+import Button from '../Button';
 
 describe('Button', () => {
-    test('Рендерится', () => {
+    it('Рендерится', () => {
         render(<Button>Какая-то кнопка</Button>);
         expect(screen.getByText('Какая-то кнопка')).toBeInTheDocument();
     });
