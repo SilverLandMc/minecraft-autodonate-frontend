750b17b9f30ba2064662fb3efa9406cc91999748
Nikita Aushev <arhonist@gmail.com>
2025-05-24 00:13:02 +0500

    Развитие карточки продукта для магазина.

--- src/pages/adminPage/hooks/useAdminInfo.ts
+++ src/pages/adminPage/hooks/useAdminInfo.ts
@@ -1,10 +1,10 @@
 import { AuthUserDto, Role } from 'app/types/api/apiTypes';
-import { setAdmin, setUserRequestFinished } from 'pages/adminPage/slices/adminPageSlice';
 import { useSelector } from 'react-redux';
 import useAppDispatch from 'shared/hooks/redux/useAppDispatch';
 import createLogger from 'shared/lib/logger/logger';
 import { request } from 'shared/lib/request/request';
 import selectAdminPagePart from 'shared/redux/selectors/selectAdminPagePart';
+import { setAdmin, setUserRequestFinished } from '@/pages/adminPage/slices/adminPageSlice';
 
 const logger = createLogger('useAdminInfo');
 
